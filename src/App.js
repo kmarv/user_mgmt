e578bfff35877e4e9d0262313b975493f06fed8b
Pieme Developer <piemedeveloper@gmail.com>
2022-06-21 11:43:38 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import React from 'react';
+import {useState} from 'react'
+import {AuthProvider} from './components/AuthContext'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
   return (
-    <div>Hello </div>
-  )
+    <Router>
+      <AuthProvider value={{currentUser}}>
+        
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../Firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-const Register = () => {
-    const [email, setEmail]= useState('');
-    const [password, setPassword] = useState('second');
-    const [confirmPassword, setConfirmPassword] = useState('');
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("second");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const validatePassword = () => {
+    let isValid = true;
+    if (password !== "" && confirmPassword !== "") {
+      if (password !== confirmPassword) {
+        isValid = false;
+        setError("Passwords does not match");
+      }
+    }
+    return isValid;
+  };
   const register = (e) => {
     e.preventDefault();
     setError("");
@@ -21,7 +33,11 @@ const Register = () => {
     setPassword("");
     setConfirmPassword("");
   };
-  return <div><form onSubmit={register} name='registration_form' ></form></div>;
-};
+  return (
+    <div>
+      <form onSubmit={register} name="registration_form"></form>
+    </div>
+  );
+}
 
 export default Register;
