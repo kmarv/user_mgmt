bf250519f923dcce685f6b08da79833ba92a064e
Pieme Developer <piemedeveloper@gmail.com>
2022-06-21 12:23:56 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import React from 'react';
-import {useState} from 'react'
+import {auth} from './firebase'
+import {onAuthStateChanged} from 'firebase/auth'
+import React, {useState, useEffect} from 'react'
 import {AuthProvider} from './components/AuthContext'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+     })
+  }, [])
 
   return (
     <Router>
